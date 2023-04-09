f2becc065b734c017ecff02f47eb698a0dbf0218
Priyanshu Garg <priyanshugarg1810@gmail.com>
2023-04-09 21:09:52 +0530

    changed the class for each image

--- proximity.js
+++ proximity.js
@@ -1,8 +1,8 @@
 // Select all elements with the class "eye"
-const eyes = document.querySelectorAll('.eye');
+const eyes = document.querySelectorAll(".eye");
 
 // Select the element with the id "anchor"
-const anchor = document.getElementById('anchor');
+const anchor = document.getElementById("anchor");
 
 // Get the size and position of the anchor element
 const rekt = anchor.getBoundingClientRect();
@@ -12,9 +12,9 @@ const anchorX = rekt.left + rekt.width / 2;
 const anchorY = rekt.top + rekt.height / 2;
 
 // When the mouse moves on the document, do this function
-document.addEventListener('mousemove', (e) => {
+document.addEventListener("mousemove", (e) => {
   // Log the mouse event to the console (just for debugging)
-  console.log(e);
+  // console.log(e);
 
   // Get the X and Y coordinates of the mouse cursor
   const mouseX = e.clientX;
@@ -27,9 +27,9 @@ document.addEventListener('mousemove', (e) => {
   console.log(angleDeg);
 
   // For each element with the class "eye", rotate it to face the mouse cursor
-  eyes.forEach(eye => {
-    eye.style.transform = `rotate(${90 + angleDeg}deg)`;
-    // anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+  eyes.forEach((eye) => {
+    eye.style.transform = ` rotate(${90 + angleDeg}deg)`;
+    anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
   });
 });
 
@@ -38,6 +38,6 @@ function angle(cx, cy, ex, ey) {
   const dy = ey - cy;
   const dx = ex - cx;
   const rad = Math.atan2(dy, dx);
-  const deg = rad * 180 / Math.PI;
+  const deg = (rad * 180) / Math.PI;
   return deg;
 }
